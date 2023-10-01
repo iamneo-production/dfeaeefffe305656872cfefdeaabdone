e29258d5bc5ed0b2454d777e5c119006ec934a33
Iamneo <techadmin@iamneo.ai>
2023-10-01 15:19:47 +0000

    Backup Commit

--- configuration/AppConfig.java
+++ configuration/AppConfig.java
@@ -1,3 +1,10 @@
+import java.beans.BeanProperty;
+
+@Configuration
+@ComponentScan(basePackages="com.exmaple");
 public class AppConfig {
-    
+    @Bean
+    public Student student(){
+        return new Student();
+    }
 }
--- configuration/Student.java
+++ configuration/Student.java
@@ -3,4 +3,5 @@ public class Student {
     private String fname;
     private String lname;
     
+
 }
